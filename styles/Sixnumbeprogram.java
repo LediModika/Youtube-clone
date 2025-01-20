630b4fc02f558a1de620bf2befb0249a60646132
Naledi Modika <naledi.modika@icloud.com>
2025-01-20 15:21:36 +0200

    first commit

---
+++ styles/Sixnumbeprogram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sixnumbeprogram {
+    public static void main (String[] args){
+        int a [6];
+
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i++) {
+            System.out.println("Please enter a whole number: ")
+        }
+
+    }
+}
